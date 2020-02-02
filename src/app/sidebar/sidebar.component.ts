385dd6e70d5eabb25f98dbf11191d8b588620037
Twinkle Prasad <tprasad@codeblock.co.in>
2020-02-02 19:00:58 +0530

    e-commerce project of lovely

---
+++ src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,106 @@
+import { Component, OnInit } from '@angular/core';
+import PerfectScrollbar from 'perfect-scrollbar';
+
+declare const $: any;
+
+//Metadata
+export interface RouteInfo {
+    path: string;
+    title: string;
+    type: string;
+    icontype: string;
+    collapse?: string;
+    children?: ChildrenItems[];
+}
+
+export interface ChildrenItems {
+    path: string;
+    title: string;
+    ab: string;
+    type?: string;
+}
+
+//Menu Items
+export const ROUTES: RouteInfo[] = [{
+        path: '/dashboard',
+        title: 'Dashboard',
+        type: 'link',
+        icontype: 'dashboard'
+    },
+    
+    {
+        path: '/forms',
+        title: 'Forms',
+        type: 'sub',
+        icontype: 'content_paste',
+        collapse: 'forms',
+        children: [
+            {path: 'regular', title: 'Regular Forms', ab:'RF'},
+            {path: 'extended', title: 'Extended Forms', ab:'EF'},
+            {path: 'validation', title: 'Validation Forms', ab:'VF'},
+            {path: 'wizard', title: 'Wizard', ab:'W'}
+        ]
+    },{
+        path: '/tables',
+        title: 'Tables',
+        type: 'sub',
+        icontype: 'grid_on',
+        collapse: 'tables',
+        children: [
+            {path: 'regular', title: 'Regular Tables', ab:'RT'},
+            {path: 'extended', title: 'Extended Tables', ab:'ET'},
+            {path: 'datatables.net', title: 'Datatables.net', ab:'DT'}
+        ]
+    },
+    
+     
+    {
+        path: '/pages',
+        title: 'Pages',
+        type: 'sub',
+        icontype: 'image',
+        collapse: 'pages',
+        children: [
+            {path: 'pricing', title: 'Pricing', ab:'P'},
+            {path: 'login', title: 'Login Page', ab:'LP'},
+            {path: 'register', title: 'Register Page', ab:'RP'},
+            {path: 'lock', title: 'Lock Screen Page', ab:'LSP'},
+            {path: 'user', title: 'User Page', ab:'UP'}
+        ]
+    }
+];
+@Component({
+    selector: 'app-sidebar-cmp',
+    templateUrl: 'sidebar.component.html',
+})
+
+export class SidebarComponent implements OnInit {
+    public menuItems: any[];
+    ps: any;
+    isMobileMenu() {
+        if ($(window).width() > 991) {
+            return false;
+        }
+        return true;
+    };
+
+    ngOnInit() {
+        this.menuItems = ROUTES.filter(menuItem => menuItem);
+        if (window.matchMedia(`(min-width: 960px)`).matches && !this.isMac()) {
+            const elemSidebar = <HTMLElement>document.querySelector('.sidebar .sidebar-wrapper');
+            this.ps = new PerfectScrollbar(elemSidebar);
+        }
+    }
+    updatePS(): void  {
+        if (window.matchMedia(`(min-width: 960px)`).matches && !this.isMac()) {
+            this.ps.update();
+        }
+    }
+    isMac(): boolean {
+        let bool = false;
+        if (navigator.platform.toUpperCase().indexOf('MAC') >= 0 || navigator.platform.toUpperCase().indexOf('IPAD') >= 0) {
+            bool = true;
+        }
+        return bool;
+    }
+}
